4fe7069858df94c1b998f961ad4b2c3ef4275ce8
revortios <61480534+revortios@users.noreply.github.com>
2024-07-17 11:13:11 +0200

    New icons

--- weather-app/src/components/Weather.jsx
+++ weather-app/src/components/Weather.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './Weather.css'
+import search_icon from '../assets/search.png'
+import clear_icon from '../assets/clear.png'
+import drizzle_icon from '../assets/drizzle.png'
+import cloud_icon from '../assets/cloud.png'
+import humidity_icon from '../assets/humidity.png'
+import rain_icon from '../assets/rain.png'
+import snow_icon from '../assets/snow.png'
+import wind_icon from '../assets/wind.png'
 
 const Weather = () => {
   return (
-    <div>Weather</div>
+    <div className='weather'>
+     <div className="search-bar">
+      <input type="text" placeholder='Search here...' />
+      <img src={search_icon} alt="" />
+     </div>
+     <img src={clear_icon} alt="" />
+     <p>16°C</p>
+    </div>
   )
 }
 
