66daea972758ede51e19b6a74c6f279af773473d
revortios <61480534+revortios@users.noreply.github.com>
2024-07-19 10:19:03 +0200

    Finished Product
    Got the API from openweathermap.org working with city call worldwide
    It currently displays the city, temperature, humidity and wind speed.
    Also the icons for all of them

--- weather-app/src/components/Weather.jsx
+++ weather-app/src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -10,14 +10,91 @@ import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 
 const Weather = () => {
+
+  // updating the location input in search bar
+  const inputRef = useRef()
+
+  const [weatherData, setWeatherData] = useState(false);
+
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02n": cloud_icon,
+    "03d": cloud_icon,
+    "03n": cloud_icon,
+    "04d": drizzle_icon,
+    "04n": drizzle_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "010d": rain_icon,
+    "010n": rain_icon,
+    "013d": snow_icon,
+    "013n": snow_icon
+  }
+
+  const search = async (city)=> {
+    if (city === "") {
+      alert("Enter City Name")
+      return;
+    }
+    try {
+      // url can be updated to do other calls, check openweathermap API calls for more functions
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
+
+      const response = await fetch(url)
+      const data = await response.json()
+
+      if(!response.ok){
+        alert(data.message);
+        return;
+      }
+
+      console.log(data);
+      const icon = allIcons[data.weather[0].icon] || clear_icon
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        icon: icon
+      })
+      console.log(weatherData.humidity);
+    } catch (error) {
+      setWeatherData(false);
+      console.error("Errpr in fetching weather data");
+    }
+  }
+
+ 
+
   return (
     <div className='weather'>
      <div className="search-bar">
-      <input type="text" placeholder='Search here...' />
-      <img src={search_icon} alt="" />
+      <input ref={inputRef} type="text" placeholder='Search here...' />
+      <img src={search_icon} alt="" onClick={()=>search(inputRef.current.value)}/>
      </div>
-     <img src={clear_icon} alt="" />
-     <p>16°C</p>
+     {weatherData?<>
+      <img src={weatherData.icon} alt="" className='weather-icon' />
+     <p className='temperature'>{weatherData.temperature}°C</p>
+     <p className='location'>{weatherData.location}</p>
+     <div className="weather-data">
+      <div className="col">
+        <img src={humidity_icon} alt="" />
+        <div>
+          <p>{weatherData.humidity}%</p>
+          <span>Humidity</span>
+        </div>
+      </div>
+      <div className="col">
+        <img src={wind_icon} alt="" />
+        <div>
+          <p>{weatherData.windSpeed}km/h</p>
+          <span>Wind Speed</span>
+        </div>
+      </div>
+     </div>
+     </>:<></>}
     </div>
   )
 }
